b46676de474148ee148730c43093df9ffcf0a7e1
Miguel <miguel.hamouic@gmail.com>
2020-07-29 13:54:57 -0400

    Config modicado

--- server/config/config.js
+++ server/config/config.js
@@ -20,12 +20,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev' // esto es una variable que
 
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//
-//urlDB = 'mongodb://localhost:27017/cafe';
-//}    else {
-urlDB = 'mongodb+srv://shouller:xKOWvXDCSKQGFtvM@cluster0.v9a0i.mongodb.net/cafe?retryWrites=true&w=majority';
-//}
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://shouller:xKOWvXDCSKQGFtvM@cluster0.v9a0i.mongodb.net/cafe?retryWrites=true&w=majority';
+}
 
 
 process.env.URLDB = urlDB;
