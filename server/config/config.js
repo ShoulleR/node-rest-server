ce57e9aecb0c9d6150654dba87be9befb8310c98
Miguel <miguel.hamouic@gmail.com>
2020-07-29 14:13:29 -0400

    Variable de Entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'dev') {
 
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://shouller:xKOWvXDCSKQGFtvM@cluster0.v9a0i.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 
